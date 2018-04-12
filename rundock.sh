c5e62c0305f111dbb7c125db22608bd046d2aa64
Sean McGroarty <mcgroas@tcd.ie>
2018-04-12 22:27:14 +0100

    udpated dockerfile and docker-compose, rundock now builds image and executs compose

--- rundock.sh
+++ rundock.sh
@@ -4,5 +4,6 @@ docker build -t $1 -f Dockerfile.base . &&
 echo "Building Stack..."
 stack --docker build &&
 echo "Creating image container..."
-stack --docker image container
+stack --docker image container &&
+docker-compose up
 echo "Done diddly done :)"
