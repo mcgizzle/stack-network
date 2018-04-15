90b62c32072d0b825057f7fd32405a4422bb9f51
Sean McGroarty <mcgroas@tcd.ie>
2018-04-15 14:14:59 +0100

    added default case for main

--- src/Main.hs
+++ src/Main.hs
@@ -7,7 +7,8 @@ import           System.Environment  (getArgs)
 import           Network.Distributed
 
 main :: IO ()
-main = do
+main =
   getArgs >>= \case
     ["build"] -> runRequestNode =<< parseNetConfig
     ["join"] -> runStackBuildT >> parseNetConfig >>= joinNetwork
+    _ -> logWarn "Bad arguments.\n COMMAND [join | build]"
