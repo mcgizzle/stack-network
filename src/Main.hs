ba7e7d5ba5b094821bff5e5daad440fbba7b5147
Sean McGroarty <mcgroas@tcd.ie>
2018-04-16 21:36:19 +0100

    using optparse applicatives for cli

--- src/Main.hs
+++ src/Main.hs
@@ -2,13 +2,46 @@
 
 module Main where
 
-import           System.Environment  (getArgs)
-
+import           Control.Monad       (join)
+import           Data.Semigroup      ((<>))
 import           Network.Distributed
+import           Options.Applicative
+
+data Opts = Opts
+  { waitNodes :: Int
+  }
+
+optParser :: Parser Opts
+optParser =
+  Opts <$>
+  option
+    auto
+    (long "nodes" <> short 'n' <>
+     help "How many nodes the Master should wait for before beginning build" <>
+     showDefault <>
+     value 1 <>
+     metavar "INT")
+
+description :: InfoMod a
+description =
+  fullDesc <>
+  progDesc "stack-network interfaces with Stack to create a distributed version" <>
+  header "stack-network"
+
+commands :: Parser (IO ())
+commands = subparser $ buildCmd <> joinCmd
+
+buildCmd :: Mod CommandFields (IO ())
+buildCmd = command "build" (info (runBuild <$> optParser) description)
+
+joinCmd :: Mod CommandFields (IO ())
+joinCmd = command "join" (info (pure runJoin) description)
+
+runJoin :: IO ()
+runJoin = runStackBuildT >> parseNetConfig >>= joinNetwork
+
+runBuild :: Opts -> IO ()
+runBuild opts = runRequestNode (waitNodes opts) =<< parseNetConfig
 
 main :: IO ()
-main =
-  getArgs >>= \case
-    ["build"] -> runRequestNode =<< parseNetConfig
-    ["join"] -> runStackBuildT >> parseNetConfig >>= joinNetwork
-    _ -> logWarn "Bad arguments.\n COMMAND [join | build]"
+main = join $ execParser (info (commands <**> helper) description)
